2c44df420ea07a994590c5fa2276f95d6306db7f
Jon Jackson <ocskier@gmail.com>
2021-06-01 11:43:20 -0400

    fix: correct auth url for google for production

--- client/src/GoogleLogin.jsx
+++ client/src/GoogleLogin.jsx
@@ -5,5 +5,13 @@ export const GoogleLogin = () => {
   const {
     state: { user },
   } = useContext(GlobalContext);
-  return <div>{!user && <a href="http://localhost:3001/auth/google">Sign In Google</a>}</div>;
+  return <div>
+      {!user && 
+        <a href={
+          process.env.NODE_ENV === 'production' ? process.env.PUBLIC_URL + "/auth/google" 
+          : "http://localhost:3001/auth/google"}>
+          Sign In Google
+        </a>
+      }
+    </div>;
 };
